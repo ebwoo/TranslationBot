ac80b9e66a557123beacd216c4ee9fa772fe396e
ebwoo <eb@wirelessprv-10-195-225-136.near.illinois.edu>
2026-09-06 14:18:05 -0500

    more stopwords

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ FRENCH_STOPWORDS = {
     # Pronouns
     "je", "tu", "il", "elle", "on", "nous", "vous", "ils", "elles",
     "me", "m", "te", "t", "se", "s", "le", "la", "les", "lui",
-    "leur", "leurs", "eux", "moi", "toi", "soi", "y", "en", "t'as", 
+    "leur", "leurs", "eux", "moi", "toi", "soi", "y", "en", "t'as", "kiffer", "avoue"
 
     # Articles / determiners
     "un", "une", "des", "du", "de", "d", "le", "la", "les", "l",
